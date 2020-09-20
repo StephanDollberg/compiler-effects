cd4d2f89d895d44bedca747d6275feb2caaead73
Stephan Dollberg <stephan.dollberg@gmail.com>
2020-09-20 20:59:55 +0000

    remove framepointer build options

--- hot-cold-function-relocation/build_hot_cold_function_relocation.sh
+++ hot-cold-function-relocation/build_hot_cold_function_relocation.sh
@@ -1,9 +1,9 @@
 set -xe;
 
-g++ -O2 -g -fno-omit-frame-pointer -std=c++17 -o base hot_cold_function_relocation.cpp;
+g++ -O2 -std=c++17 -o base hot_cold_function_relocation.cpp;
 
 rm -rf hot_cold_function_relocation.gcda
-g++ -O2 -g -fno-omit-frame-pointer -std=c++17 -fprofile-generate -o pgo hot_cold_function_relocation.cpp
+g++ -O2 -std=c++17 -fprofile-generate -o pgo hot_cold_function_relocation.cpp
 ./pgo 1
-g++ -O2 -g -fno-omit-frame-pointer -std=c++17 -fprofile-use -o pgo hot_cold_function_relocation.cpp
+g++ -O2 -std=c++17 -fprofile-use -o pgo hot_cold_function_relocation.cpp
 
