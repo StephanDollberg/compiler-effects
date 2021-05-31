35fb8e8d5f223843154259d5c50b10f23ab81989
Stephan Dollberg <stephan.dollberg@gmail.com>
2021-05-31 16:36:33 +0100

    Use -O3 instead

--- devirtualize/build_devirtualize.sh
+++ devirtualize/build_devirtualize.sh
@@ -1,9 +1,9 @@
 set -xe;
 
-g++ -O2 -std=c++17 -o base devirtualize.cpp;
+g++ -O3 -std=c++17 -o base devirtualize.cpp;
 
 rm -rf pgo-devirtualize.gcda
-g++ -O2 -std=c++17 -fprofile-generate -o pgo devirtualize.cpp
+g++ -O3 -std=c++17 -fprofile-generate -o pgo devirtualize.cpp
 ./pgo 1
-g++ -O2 -std=c++17 -fprofile-use -o pgo devirtualize.cpp
+g++ -O3 -std=c++17 -fprofile-use -o pgo devirtualize.cpp
 
--- hot-cold-function-splitting/build_hot_cold_function_splitting.sh
+++ hot-cold-function-splitting/build_hot_cold_function_splitting.sh
@@ -1,9 +1,9 @@
 set -xe;
 
-g++ -O2 -std=c++17 -o base hot_cold_function_splitting.cpp;
+g++ -O3 -std=c++17 -o base hot_cold_function_splitting.cpp;
 
-rm -rf hot_cold_function_splitting.gcda
-g++ -O2 -std=c++17 -fprofile-generate -o pgo hot_cold_function_splitting.cpp
+rm -rf pgo-hot_cold_function_splitting.gcda
+g++ -O3 -std=c++17 -fprofile-generate -o pgo hot_cold_function_splitting.cpp
 ./pgo 0
-g++ -O2 -std=c++17 -fprofile-use -o pgo hot_cold_function_splitting.cpp
+g++ -O3 -std=c++17 -fprofile-use -o pgo hot_cold_function_splitting.cpp
 
