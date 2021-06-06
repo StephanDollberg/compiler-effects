c86dd2645cd246dff3ee67c5b553d54d4ddb5241
Stephan Dollberg <stephan.dollberg@gmail.com>
2021-06-06 12:46:12 +0100

    Make test scripts have proper header

--- hot-cold-function-splitting/test_hot_cold_function_splitting.sh
+++ hot-cold-function-splitting/test_hot_cold_function_splitting.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 set -xe;
 perf stat -e L1-icache-load-misses,br_misp_retired.all_branches -r 10 ./base 0
 perf stat -e L1-icache-load-misses,br_misp_retired.all_branches -r 10 ./pgo 0
